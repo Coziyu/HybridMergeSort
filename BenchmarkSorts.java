61ff43c51ec9b8f5a037abbe690a78a628a826c4
Coziyu <59217639+Coziyu@users.noreply.github.com>
2024-09-04 21:58:58 +0800

    Clean up prints and added 10mil bench

--- BenchmarkSorts.java
+++ BenchmarkSorts.java
@@ -26,47 +26,82 @@ public class BenchmarkSorts {
         return arr;
     }
     public static void main(String[] args) {
-        int largestElement = 10000;
-
-        ArrayList<int[]> testCases = new ArrayList<int[]>();
-        for(int n = 1; n <= 10000000; n*=10){
-            testCases.add(randIntArray(n, largestElement));
-        }
+        int largestElement = 1000000;
 
         // i)
         // Arbitraily select an array size to switch from merge to insertion sort.
-        System.out.println("Benchmark: fix threshold, vary input length:");
+        ArrayList<int[]> testCases = new ArrayList<int[]>();
+        for(int n = 1000; n <= 10000000; n*=10){
+            testCases.add(randIntArray(n, largestElement));
+        }
+        System.out.println("i) Benchmark: Fix threshold S = 4, vary Array Length:");
+        System.out.println("Array Length, Key Comps, Time (ms)");
         int insertionSortThreshold = 4;
         for (int[] testCase : testCases) {
             int[] input = testCase.clone();
             long startTime = System.nanoTime();
             long keyComparisons = MergeSort.sortHybrid(input, 0, input.length - 1, insertionSortThreshold);
             long endTime = System.nanoTime();
-            System.out.printf("%-9s %-11s %d%n", input.length + ",", keyComparisons + ", ", (endTime - startTime));
+            System.out.printf("%-9s %-11s %10.5f%n", input.length + ",", keyComparisons + ", ", (double)(endTime - startTime)/1000000);
         }
 
         // ii)
-        // We test with size = 10000
-        int fixedLength = 10000, numSamples = 10;
+        // We test with size = 100000
+        int fixedLength = 100000, numSamples = 100;
         testCases = new ArrayList<int[]>();
         for(int n = 0; n < numSamples; n++){
             testCases.add(randIntArray(fixedLength, largestElement));
         }
-        System.out.println("\n\nBenchmark: vary threshold, fix input length:");
+        System.out.println("\n\nii) Benchmark: Fix Array Length = 100,000, vary threshold S, average of " + numSamples + " samples:");
+        System.out.println("Threshold, Key Comps, Time (ms)");
         for(insertionSortThreshold = 0; insertionSortThreshold <= 100; insertionSortThreshold++){
-            double averageKeyComparisons = 0;
-            long averageTimeElapsed = 0;
+            long averageKeyComparisons = 0;
+            double averageTimeElapsed = 0;
             for (int[] testCase : testCases) {
                 int[] input = testCase.clone();
                 long startTime = System.nanoTime();
                 long keyComparisons = MergeSort.sortHybrid(input.clone(), 0, input.length - 1, insertionSortThreshold);
                 long endTime = System.nanoTime();
                 averageKeyComparisons += keyComparisons;
-                averageTimeElapsed += endTime - startTime;
+                averageTimeElapsed += (double)(endTime - startTime)/1000000;
             }
             averageKeyComparisons /= numSamples;
             averageTimeElapsed /= numSamples;
-            System.out.printf("%-3s %-11s %s\n", insertionSortThreshold + ",", averageKeyComparisons + ", ", averageTimeElapsed);
+            System.out.printf("%-4s %-11s %10.5f\n", insertionSortThreshold + ",", averageKeyComparisons + ", ", averageTimeElapsed);
+        }
+
+
+        // iii)?
+        // 10,000,000 size, naive merge vs threshold = 6
+        numSamples = 10;
+        testCases = new ArrayList<int[]>();
+        for(int n = 0; n < numSamples; n++){
+            testCases.add(randIntArray(10000000, largestElement));
+        }
+        System.out.println("\n\niii) Benchmark: Compare Classic Mergesort, and Hybrid Mergesort with threshold S = 6, average of " + numSamples + " samples:");
+        System.out.println("Threshold, Key Comps, Time (ms)");
+        for(insertionSortThreshold = 0; insertionSortThreshold <= 6; insertionSortThreshold += 6){
+            long averageKeyComparisons = 0;
+            double averageTimeElapsed = 0;
+            for (int[] testCase : testCases) {
+                int[] input = testCase.clone();
+                long startTime, endTime, keyComparisons;
+                if(insertionSortThreshold == 0){
+                    startTime = System.nanoTime();
+                    keyComparisons = MergeSort.sort(input.clone(), 0, input.length - 1);
+                    endTime = System.nanoTime();        
+                }
+                else{
+                    startTime = System.nanoTime();
+                    keyComparisons = MergeSort.sortHybrid(input.clone(), 0, input.length - 1, insertionSortThreshold);
+                    endTime = System.nanoTime();
+                }
+                averageKeyComparisons += keyComparisons;
+                averageTimeElapsed += (double)(endTime - startTime)/1000000;
+            }
+            averageKeyComparisons /= numSamples;
+            averageTimeElapsed /= numSamples;
+            System.out.printf("%-4s %-11s %10.5f\n", insertionSortThreshold + ",", averageKeyComparisons + ", ", averageTimeElapsed);
         }
     }    
 }
